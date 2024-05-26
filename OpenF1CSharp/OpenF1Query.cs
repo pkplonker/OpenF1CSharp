395ad4a85c662636daf04671c80d791bb8bfbb40
stuart <pkplonker@gmail.com>
2024-05-26 23:05:50 +0100

    Query Setup

---
+++ OpenF1CSharp/OpenF1Query.cs
@@ -0,0 +1,7 @@
+﻿namespace OpenF1CSharp;
+
+public interface IOpenF1Query
+{
+	public string GenerateQuery();
+	public void Reset();
+}
---
+++ OpenF1CSharp/QueryBuilder.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+
+public enum ComparisonOperator
+{
+	Equal,
+	GreaterThan,
+	GreaterThanOrEqual,
+	LessThan,
+	LessThanOrEqual
+}
+
+public class QueryBuilder<T>
+{
+	private readonly Type type;
+	private readonly List<string> filters;
+	private string baseQuery;
+
+	public QueryBuilder(string baseQuery)
+	{
+		type = typeof(T);
+		filters = new List<string>();
+		this.baseQuery = baseQuery;
+	}
+
+	public void Reset(string baseQuery)
+	{
+		filters.Clear();
+		this.baseQuery = baseQuery;
+	}
+
+	public QueryBuilder<T> Filter<TValue>(string propertyName, TValue value,
+		ComparisonOperator op = ComparisonOperator.Equal)
+	{
+		var jsonPropertyName = GetPropertyName(propertyName);
+		var filter = GetFilterString(jsonPropertyName, value, op);
+		filters.Add(filter);
+		return this;
+	}
+
+	public string Build()
+	{
+		var query = baseQuery;
+		if (filters.Any())
+		{
+			query += "&" + string.Join("&", filters);
+		}
+
+		return query;
+	}
+
+	private string GetPropertyName(string propertyName)
+	{
+		var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance);
+		if (property == null)
+		{
+			throw new ArgumentException($"Property '{propertyName}' not found on type '{type.FullName}'");
+		}
+
+		var attribute = property.GetCustomAttribute<JsonPropertyAttribute>();
+		return attribute?.PropertyName ?? propertyName;
+	}
+
+	private string GetFilterString<TValue>(string propertyName, TValue value, ComparisonOperator op)
+	{
+		return op switch
+		{
+			ComparisonOperator.Equal => $"{propertyName}={value}",
+			ComparisonOperator.GreaterThan => $"{propertyName}>{value}",
+			ComparisonOperator.GreaterThanOrEqual => $"{propertyName}>={value}",
+			ComparisonOperator.LessThan => $"{propertyName}<{value}",
+			ComparisonOperator.LessThanOrEqual => $"{propertyName}<={value}",
+			_ => throw new ArgumentException("Invalid comparison operator")
+		};
+	}
+	
+	
+}
