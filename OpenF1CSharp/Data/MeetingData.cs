0e98e7fdf787a3d2aeec74dc2b667ceefbc5841b
stuart <pkplonker@gmail.com>
2024-05-27 23:01:45 +0100

    Updated Meetings Data struct

--- OpenF1CSharp/Data/MeetingData.cs
+++ OpenF1CSharp/Data/MeetingData.cs
@@ -4,18 +4,39 @@ namespace OpenF1CSharp;
 
 public struct MeetingData
 {
-	[JsonProperty("meeting_key")]
-	public int? MeetingKey { get; set; }
+    [JsonProperty("circuit_key")]
+    public int? CircuitKey { get; set; }
 
-	[JsonProperty("meeting_name")]
-	public string? MeetingName { get; set; }
+    [JsonProperty("circuit_short_name")]
+    public string? CircuitShortName { get; set; }
 
-	[JsonProperty("circuit_key")]
-	public int? CircuitKey { get; set; }
+    [JsonProperty("country_code")]
+    public string? CountryCode { get; set; }
 
-	[JsonProperty("year")]
-	public int? Year { get; set; }
+    [JsonProperty("country_key")]
+    public int? CountryKey { get; set; }
 
-	[JsonProperty("session_keys")]
-	public List<int>? SessionKeys { get; set; }
+    [JsonProperty("country_name")]
+    public string? CountryName { get; set; }
+
+    [JsonProperty("date_start")]
+    public DateTime? DateStart { get; set; }
+
+    [JsonProperty("gmt_offset")]
+    public string? GmtOffset { get; set; }
+
+    [JsonProperty("location")]
+    public string? Location { get; set; }
+
+    [JsonProperty("meeting_key")]
+    public int? MeetingKey { get; set; }
+
+    [JsonProperty("meeting_name")]
+    public string? MeetingName { get; set; }
+
+    [JsonProperty("meeting_official_name")]
+    public string? MeetingOfficialName { get; set; }
+
+    [JsonProperty("year")]
+    public int? Year { get; set; }
 }
